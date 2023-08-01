1fb5af832829ca76662dae22388acb53b9faf71b
GABRIEL FELLIPE MARQUARDT <gabriel_marquardt@estudante.sesisenai.org.br>
2023-08-01 16:06:46 -0300

    90%

--- components/BotoesComponent.jsx
+++ components/BotoesComponent.jsx
@@ -1,40 +1,65 @@
+import React, { useState } from 'react';
+import VisorComponent from './VisorComponent'
+
 export default function Botoes() {
+    const [input, setInput] = useState("");
+
+    function verificaMais(input, setInput) {
+        if (input.length > 0 && !(input[input.length - 1] === '+') && !(input[input.length - 1] === '-')) {
+            setInput(input + '+');
+        }
+    }
+
+
+    function verificaMenos(input, setInput) {
+        if (input.length > 0 && !(input[input.length - 1] === '-') && !(input[input.length - 1] === '+')) {
+            setInput(input + '-');
+        }
+    }
+
+    function resultado(input, setInput) {
+        const resultado = eval(input)
+        setInput(resultado.toString())
+    }
     return (
-        <div>
-            <button onClick={() => pegarClick(1)}>1</button>
-            <button onClick={() => pegarClick(2)}>2</button>
-            <button onClick={() => pegarClick(3)}>3</button>
-            <button onClick={() => pegarClick(4)}>4</button>
-            <button onClick={() => pegarClick(5)}>5</button>
-            <button onClick={() => pegarClick(6)}>6</button>
-            <button onClick={() => pegarClick(7)}>7</button>
-            <button onClick={() => pegarClick(8)}>8</button>
-            <button onClick={() => pegarClick(9)}>9</button>
+        <>
+            <div className='flex justify-center justify-items-center'>
+                <VisorComponent input={input} />
+            </div>
 
-            <button onClick={() => soma('+')}>+</button>
-            <button onClick={() => subtra('-')}>-</button>
-            <button onClick={() => resultado('=')}>=</button>
-            <button onClick={() => limpa('Clear')}>Clear</button>
-        </div>
+
+            <div className='w-full flex space-x-1 flex flex-col flex items-center'>
+                <div className='flex  flex-col items-center bg-slate-500 w-40 p-4'>
+
+                    <div>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "1")}>1</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "2")}>2</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "3")}>3</button>
+                    </div>
+                    <div>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "4")}>4</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "5")}>5</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "6")}>6</button>
+                    </div>
+                    <div>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "7")}>7</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "8")}>8</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "9")}>9</button>
+                    </div>
+                    <div>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput(input + "0")}>0</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => verificaMais(input, setInput)}>+</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => verificaMenos(input, setInput)}>-</button>
+                    </div>
+
+                    <div>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => resultado(input, setInput)}>=</button>
+                        <button className='rounded-full bg-blue-100 w-10 h-10' onClick={() => setInput("")}>Clear</button>
+                    </div>
+                </div>
+            </div>
+
+        </>
+
     )
-}
-
-function pegarClick(numero) {
-    console.log(numero)
-}
-
-function soma(funcao) {
-console.log(funcao)
-}
-
-function subtra(funcao) {
-    console.log(funcao)
-}
-
-function resultado(funcao) {
-    console.log(funcao)
-}
-
-function limpa(funcao) {
-    console.log(funcao)
 }
--- pages/basicos/calculadora.jsx
+++ pages/basicos/calculadora.jsx
@@ -3,8 +3,10 @@ import BotoesComponent from "../../components/BotoesComponent";
 export default function calculadora() {
     return (
         <div>
-            <h1>Calculadora</h1>
+
             <BotoesComponent />
         </div>
+
+        
     )
 }
